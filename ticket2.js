d14eb3bad583a724280c84e091304e3523e5ea6f
jordxx <danieljordan553@gmail.com>
2022-06-24 12:10:46 +0700

    modified object

--- ticket2.js
+++ ticket2.js
@@ -1,54 +1,46 @@
-
-const nama = document.getElementsByClassName('name') ;
-const tanggal = document.getElementsByClassName('date') ;
-const lok = document.getElementsByClassName('location') ;
-const desc = document.getElementsByClassName('desc') ;
-const harga = document.getElementsByClassName('price') ;
-const button1 = document.getElementById('button1') ;
-const button2 = document.getElementById('button2') ;
-const button3 = document.getElementById('button3') ;
-
-
-    
-
-    
+const nama = document.getElementsByClassName("name");
+const tanggal = document.getElementsByClassName("date");
+const lok = document.getElementsByClassName("location");
+const desc = document.getElementsByClassName("desc");
+const harga = document.getElementsByClassName("price");
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
 
 let resultFix = [
-    {
-        name: "Sule",
-        date: "-1 December 2022",
-        time: "Rabu, 11.00 WIB",
-        location: "Bogor",
-        desc: "lorem ipsum",
-        tipe: [
-            300000, 200000, 100000
-        ]
-    }
-]
+  {
+    name: "Sule",
+    date: "11 December 2022",
+    time: "Rabu, 11.00 WIB",
+    location: "Bogor",
+    desc: "Sutisna, more commonly known as Sule, is an Indonesian comedian and actor. He became well-known after winning the Indonesian comedian audition show API with Ogi Suwarna and Obin Wahyudin in the group SOS in 2005.",
+    tipe: [300000, 200000, 100000],
+  },
+];
 console.log(resultFix);
 
-let price = resultFix[0]['tipe'] ;
+let price = resultFix[0]["tipe"];
 for (const a of resultFix) {
-    for (let i = 0; i < nama.length; i++) {
-        const element = nama[i];
-        element.firstChild.textContent = a.name
-    }
-    for (let i = 0; i < tanggal.length; i++) {
-        const element = tanggal[i];
-        element.textContent = a.date
-    }
-    for (let i = 0; i < lok.length; i++) {
-        const element = lok[i];
-        element.textContent = a.location
-    }
-    for (let i = 0; i < desc.length; i++) {
-        const element = desc[i];
-        element.innerText = a['desc']
-    }
-    for (let i = 0; i < harga.length; i++) {
-        const element = harga[i];
-        element.innerText = price[i]
-    }
+  for (let i = 0; i < nama.length; i++) {
+    const element = nama[i];
+    element.firstChild.textContent = a.name;
+  }
+  for (let i = 0; i < tanggal.length; i++) {
+    const element = tanggal[i];
+    element.textContent = a.date;
+  }
+  for (let i = 0; i < lok.length; i++) {
+    const element = lok[i];
+    element.textContent = a.location;
+  }
+  for (let i = 0; i < desc.length; i++) {
+    const element = desc[i];
+    element.innerText = a["desc"];
+  }
+  for (let i = 0; i < harga.length; i++) {
+    const element = harga[i];
+    element.innerText = price[i];
+  }
 }
 // let price = objKecil['tipe'] ;
 // console.log(price.length);
@@ -72,29 +64,32 @@ for (const a of resultFix) {
 //     const element = harga[i];
 //     element.innerText = price[i]
 // }
-let totalPrice = 0
-let numberVVIP = 0 ;
-button1.onclick = function() {myFunction1()};
+let totalPrice = 0;
+let numberVVIP = 0;
+button1.onclick = function () {
+  myFunction1();
+};
 function myFunction1() {
-    numberVVIP++ ;
-    totalPrice += Number(harga[0].innerText)
-    console.log(numberVVIP, totalPrice, 'ini VVIP');
-    resizeTo.onclick
+  numberVVIP++;
+  totalPrice += Number(harga[0].innerText);
+  console.log(numberVVIP, totalPrice, "ini VVIP");
+  resizeTo.onclick;
 }
-let numberVIP = 0 ;
-button2.onclick = function() {myFunction2()};
+let numberVIP = 0;
+button2.onclick = function () {
+  myFunction2();
+};
 function myFunction2() {
-    numberVIP++ ;
-    totalPrice += Number(harga[1].innerText)
-    console.log(numberVIP, totalPrice,  'ini VIP');
+  numberVIP++;
+  totalPrice += Number(harga[1].innerText);
+  console.log(numberVIP, totalPrice, "ini VIP");
 }
-let numberReg = 0 ;
-button3.onclick = function() {myFunction3()};
+let numberReg = 0;
+button3.onclick = function () {
+  myFunction3();
+};
 function myFunction3() {
-    numberReg++ ;
-    totalPrice += Number(harga[2].innerText)
-    console.log(numberReg, totalPrice, 'ini Reg');
+  numberReg++;
+  totalPrice += Number(harga[2].innerText);
+  console.log(numberReg, totalPrice, "ini Reg");
 }
-
-
-
